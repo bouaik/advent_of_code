4bf51b5bd3569824921d66dad53b4051bacb732a
bouaik <lhoussaine.bouaik@um6p.ma>
2024-10-10 16:15:23 +0100

    day 2 - part 2

--- 2015/ruby/2.rb
+++ 2015/ruby/2.rb
@@ -1,15 +1,28 @@
+# part 1
+# sum = 0
 
+# File.foreach("./2015/2.txt") do |line|
+#   l, w, h = line.split("x")
+#   l, w , h = l.to_i, w.to_i, h.chomp.to_i
+
+#   min = [l*h, h*w, l*w].min
+#   surface = 2*(l*h + h*w + l*w) + min
+
+#   sum += surface
+# end
+# puts sum
+
+
+# part 2
 sum = 0
-
 File.foreach("./2015/2.txt") do |line|
   l, w, h = line.split("x")
   l, w , h = l.to_i, w.to_i, h.chomp.to_i
+  min1, min2 = [l, h, w].min(2)
 
-  min = [l*h, h*w, l*w].min
-  surface = 2*(l*h + h*w + l*w) + min
+  surface = l*w*h
+  surface += 2*(min1 + min2)
 
   sum += surface
 end
-
-
 puts sum
