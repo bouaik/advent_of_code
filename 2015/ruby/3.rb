afa59d2b3fe56f7a7df8876efbf9b50bdba01295
bouaik <lhoussaine.bouaik@um6p.ma>
2024-10-10 20:49:50 +0100

    day 3 - part 1

---
+++ 2015/ruby/3.rb
@@ -0,0 +1,32 @@
+cords = [0, 0]
+houses = { cords.dup => 1 }
+
+File.foreach("./2015/3.txt") do |line|
+  line.each_char do |d|
+    if houses.key?(cords)
+      houses[cords] += 1
+    else
+      houses[cords.dup] = 1
+    end
+
+    if d == "^"
+      cords[0] += 1
+    elsif d == "v"
+      cords[0] -= 1
+    elsif d == ">"
+      cords[1] += 1
+    elsif d == "<"
+      cords[1] -= 1
+    end
+  end
+
+  if houses.key?(cords)
+    houses[cords] += 1
+  else
+    houses[cords.dup] = 1
+  end
+end
+
+sum =  houses.values.filter {|n| n >= 1}
+
+p sum.length
