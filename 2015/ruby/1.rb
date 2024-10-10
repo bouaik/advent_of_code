a4ed9504865f99d5b6f0f858ce9a34b00dce343e
bouaik <lhoussaine.bouaik@um6p.ma>
2024-10-10 15:57:13 +0100

    day 1 redo

--- 2015/ruby/1.rb
+++ 2015/ruby/1.rb
@@ -1,1 +1,22 @@
-puts "it was easy"
+File.foreach("./2015/1.txt") do |line|
+
+  n = 0
+  line.each_char.with_index do |c, i|
+
+
+    # start part two
+    if n == -1
+      puts i
+      break
+    end
+    # end part two
+
+    if c == "("
+      n += 1
+    elsif c == ")"
+      n -= 1
+    end
+  end
+
+  puts n
+end
