5465893fbb6adc808a6a7606388d8babaea6bfb3
bouaik <lhoussaine.bouaik@um6p.ma>
2024-10-12 01:24:27 +0100

    refactor day 3

--- 2015/ruby/3.rb
+++ 2015/ruby/3.rb
@@ -28,53 +28,35 @@
 # p sum.length
 
 
-
-
 ####################### PART TWO #######################
+
+
 santa_cords = [0, 0]
 robot_cords = [0, 0]
 houses = { santa_cords.dup => 1 }
 
-File.foreach("./2015/3.txt") do |line|
-  line.each_char.with_index do |d, i|
-    if i % 2 == 0 # santa moves
-      if houses.key?(santa_cords)
-        houses[santa_cords] += 1
-      else
-        houses[santa_cords.dup] = 1
-      end
-      if d == "^"
-        santa_cords[0] += 1
-      elsif d == "v"
-        santa_cords[0] -= 1
-      elsif d == ">"
-        santa_cords[1] += 1
-      elsif d == "<"
-        santa_cords[1] -= 1
-      end
-    else  # robot moves
-      if houses.key?(robot_cords)
-        houses[robot_cords] += 1
-      else
-        houses[robot_cords.dup] = 1
-      end
-      if d == "^"
-        robot_cords[0] += 1
-      elsif d == "v"
-        robot_cords[0] -= 1
-      elsif d == ">"
-        robot_cords[1] += 1
-      elsif d == "<"
-        robot_cords[1] -= 1
-      end
-    end
+def moves(cords, houses, d)
+  if houses.key?(cords)
+    houses[cords] += 1
+  else
+    houses[cords.dup] = 1
+  end
+  if d == "^"
+    cords[0] += 1
+  elsif d == "v"
+    cords[0] -= 1
+  elsif d == ">"
+    cords[1] += 1
+  elsif d == "<"
+    cords [1] -= 1
   end
 end
 
-
-def moves
-
-
+File.foreach("./2015/3.txt") do |line|
+  line.each_char.with_index do |d, i|
+    i % 2 == 0 ? moves(santa_cords, houses, d) : moves(robot_cords, houses, d)
+  end
+end
 
 sum =  houses.values.filter {|n| n >= 1}
 
